4ea05a4386776c353b369b3fd4fe591ac40850f9
de-andrei <60333523+de-andrei@users.noreply.github.com>
2026-03-14 19:02:53 +0300

    Update const.py

--- custom_components/de_ble_blood_pressure_monitor/const.py
+++ custom_components/de_ble_blood_pressure_monitor/const.py
@@ -7,10 +7,10 @@ DOMAIN = "de_ble_blood_pressure_monitor"
 DEVICE_MANUFACTURER = "Medisana"
 DEVICE_MODEL = "BU 575"
 
-# Service UUIDs (из вашего ESPHome конфига)
+# Service UUIDs
 BP_SERVICE_UUID = "00001810-0000-1000-8000-00805f9b34fb"
 BP_CHAR_UUID = "00002a35-0000-1000-8000-00805f9b34fb"
 
-# Update intervals - УМЕНЬШАЕМ для более быстрого обнаружения!
-SCAN_INTERVAL = timedelta(seconds=2)  # Было 3, стало 2
-CONNECT_TIMEOUT = 5  # Было 10, стало 5
+# Update intervals
+SCAN_INTERVAL = timedelta(seconds=3)
+CONNECT_TIMEOUT = 10
