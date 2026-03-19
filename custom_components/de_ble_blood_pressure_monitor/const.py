c0faa943c5bb36a45ceecfb4e14fd77f165938d2
de-andrei <60333523+de-andrei@users.noreply.github.com>
2026-03-20 00:36:15 +0300

    Update const.py

--- custom_components/de_ble_blood_pressure_monitor/const.py
+++ custom_components/de_ble_blood_pressure_monitor/const.py
@@ -12,5 +12,5 @@ BP_SERVICE_UUID = "00001810-0000-1000-8000-00805f9b34fb"
 BP_CHAR_UUID = "00002a35-0000-1000-8000-00805f9b34fb"
 
 # Update intervals - можно увеличить, так как полагаемся на callback
-SCAN_INTERVAL = timedelta(seconds=20)
+SCAN_INTERVAL = timedelta(seconds=10)
 CONNECT_TIMEOUT = 10
